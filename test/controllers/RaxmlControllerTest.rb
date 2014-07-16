36473758c6d9222f8412f6f3851f61cec7450599
woetzens <nils.woetzel@h-its.org>
2014-07-16 17:25:30 +0200

    added testing for RaxmlController

---
+++ test/controllers/RaxmlControllerTest.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class RaxmlControllerTest < ActionController::TestCase
+  test "get index" do
+    get :index
+    assert_response :success
+  end
+  
+  test "get submit_single_gene" do
+    get :submit_single_gene
+    assert_response :success
+  end
+
+  test "get submit_multiple_gene" do
+    get :submit_multiple_gene
+    assert_response :success
+  end
+end
